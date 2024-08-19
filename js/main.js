d7b6e27d4f1dcfe21621ae42c2025399f641f0d1
Leoledes <ledesma.leonardoa@gmail.com>
2024-08-19 17:34:38 -0300

    continuar en menú

--- js/main.js
+++ js/main.js
@@ -5,14 +5,29 @@ let id = parseInt(prompt("Ingrese su id de empleado"))
 console.log(nombreUsuario, email, sector, id)
 alert("Bienvenido al sistema de tickets, confirme si los datos ingresados son correctos: \n "+nombreUsuario+" \n "+email+" \n "+sector+" \n "+id)
 
-let menu = parseInt(prompt("Ingrese 1 Para realizar un ticket nuevo, 2 Para consultar el estado de un ticket abierto, 3 Para hablar con un técnico, * para salir"))
-switch(menu){
-    case 1:
-        console.log("Que tipo de incidente afecta su equipo?")
-    case 2:
-        console.log("Ingrese el número de ticket que abrió con anterioridad")
-    case 3:
-        console.log("Un técnico se contactará a la brevedad")
-    case 4:
-        console.log("Volverá el Menú principal")
+
+
+let continuar = true
+while (continuar){
+    let menu = parseInt(prompt("Ingrese 1 Para realizar un ticket nuevo, 2 Para consultar el estado de un ticket abierto, 3 Para hablar con un técnico, * para salir"))
+    switch(menu){
+        case 1:
+            console.log("Que tipo de incidente afecta su equipo?")
+            break
+        case 2:
+            console.log("Ingrese el número de ticket que abrió con anterioridad")
+            break
+        case 3:
+            console.log("Un técnico se contactará a la brevedad")
+            break
+        case 4:
+            console.log("Volverá al Menú principal")
+            break
+    }
+
+    let confirmar = prompt("Otra consulta? (SI/NO)").toLowerCase()
+    if(confirmar == "no") {
+        continuar = false
+        console.log("Saludos")
+    }
 }
